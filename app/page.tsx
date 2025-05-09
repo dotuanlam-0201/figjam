cf46bf57c1b634845e109cc38785e1f81f768d52
dotuanlam-0201 <dotuanlam212k@gmail.com>
2025-05-09 17:20:34 +0700

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,43 @@
 "use client"
 
+import Konva from "konva"
+import React, { useEffect, useRef, useState } from "react"
 import { Stage } from "react-konva"
+const WIDTH = 3000
+const HEIGHT = 3000
+const NUMBER = 200
 
 export default function Home() {
+  const stageRef = useRef(null as Konva.Stage | null)
+  const containerRef = useRef<null | HTMLDivElement>(null)
+  const [container, setContainer] = useState({
+    width: 0,
+    height: 0,
+  })
+  const [nodes] = React.useState(() =>
+    Array(NUMBER).map(() => ({
+      x: WIDTH * Math.random(),
+      y: HEIGHT * Math.random(),
+    }))
+  )
+  useEffect(() => {
+    if (containerRef.current) {
+      setContainer({
+        width: containerRef.current.clientWidth,
+        height: containerRef.current.clientHeight,
+      })
+    }
+  }, [containerRef.current])
+
   return (
-    <Stage
-      style={{
-        background: "#f5f5f5",
-        backgroundImage: "radial-gradient(#C4C4C4 1px, transparent 0)",
-        backgroundSize: "20px 20px",
-      }}
-      width={window.innerWidth}
-      height={window.innerHeight}
-    ></Stage>
+    <main ref={containerRef} className="w-screen h-screen">
+      <Stage
+        ref={stageRef}
+        draggable
+        className="bg-white bg-[radial-gradient(#e5e7eb_1px,transparent_1px)] [background-size:16px_16px]"
+        width={container.width}
+        height={container.height}
+      ></Stage>
+    </main>
   )
 }
